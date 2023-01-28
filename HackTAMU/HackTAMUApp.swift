b8eb25ed805e086b132d715bb463fcb702f6877a
itsAmyHong <itsAmyHong@gmail.com>
2023-01-28 14:22:43 -0600

    Initial Commit

---
+++ HackTAMU/HackTAMUApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackTAMUApp.swift
+//  HackTAMU
+//
+//  Created by Amy Hong on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HackTAMUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
